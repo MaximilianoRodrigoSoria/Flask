80e85d8a862ae09df0b7e0d2272366f3dadc5ce5
vagrant <maximilianorodrigosoria@gmail.com>
2018-04-12 14:05:39 +0200

    templates

--- 03-Routes2/routes.py
+++ 03-Routes2/routes.py
@@ -13,16 +13,7 @@ def index():
 def saludo(nombre='valor por defecto',apellido='',edad=0):
 	return "Hola {}, {}, {}".format(nombre,apellido,edad)
 
-@app.route("/params")
-def params():
-	param = request.args.get('param1','default param')
-	return "El parametro es {}".format(param) 
 
-@app.route("/twoparams")
-def params2():
-	param = request.args.get('param1','default param')
-	param2= request.args.get('param2','default param2')
-	return "El parametro es {} , {}".format(param,param2) 
 
 
 
